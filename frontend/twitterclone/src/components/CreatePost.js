e8c986d9f38becd17dcca74afc4c15e051005c96
duongwthuj <thunguyenduong37@gmail.com>
2025-02-18 18:02:21 +0700

    setup the enviroment and Home Page Ui

--- frontend/twitterclone/src/components/CreatePost.js
+++ frontend/twitterclone/src/components/CreatePost.js
@@ -20,7 +20,7 @@ function CreatePost() {
           <div className="flex items-center space-x-4 p-4 border-b border-gray-200">
             <div>
               <Avatar
-                src="https://scontent.fhan14-5.fna.fbcdn.net/v/t39.30808-6/472989512_1651118499176427_2891168160856902537_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeE4hrTpVYLa5JBc4hD3hg_j69Arnnv--bHr0Cuee_75sVooeinYV3KsPr-Ib0gdA43fjEos2hjhlZ4hMEMDgWFD&_nc_ohc=QbkfyF3Y2k0Q7kNvgGqhIr5&_nc_oc=AdjHjrAXyaZWII_dbViKJHYibRb-XipRNSOzBXNLk_qwWuLwXhIUPl8wc7Rtx-dnqEU&_nc_zt=23&_nc_ht=scontent.fhan14-5.fna&_nc_gid=AQjMS2fMaS3Cp8dygb9ZuXn&oh=00_AYC9ulZoFfzaINasK83IGR1EUrP-X_5L6YrFydx2lbiuOw&oe=67BA3273"
+                src="https://images-workbench.99static.com/BwVnVkeaKVEFCm0j5dbUSG7rAJU=/99designs-contests-attachments/97/97041/attachment_97041107"
                 size="54"
                 round={true}
               />
